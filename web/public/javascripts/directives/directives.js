63fd309726e66874ddbfa995d748f2f9cd3e0210
lirco <lirco77@gmail.com>
2014-06-02 22:39:18 +0300

    Removed use in directive for drag and drop functionality

--- web/public/javascripts/directives/directives.js
+++ web/public/javascripts/directives/directives.js
@@ -30,16 +30,6 @@ define(
         '</article>'+
       '</div>'
 
-//      '<div class="page_item" ng-mouseenter="editing=true" ng-mouseleave="editing=false">' +
-//        '<div class="page_item_title">' +
-//          '<a href="{{item.url}}" title="{{item.url}}"> {{item.title}} </a>' +
-//          '<div ng-show="editing" ng-click="deleteItem({itemId:item._id})"> <span>Delete</span></div>' +
-//        '</div>' +
-//        '<div class="page_item_image" ng-show="item.images.length > 0">' +
-//          '<img width=380, height=100, ng-src={{item.images[0].url}}>' +
-//        '</div>' +
-//      '</div>';
-
 
     return {
       restrict: 'E',
