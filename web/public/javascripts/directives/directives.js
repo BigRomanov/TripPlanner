0b873e8824f426bde1030e3bb685273ccba3ac48
lirco <lirco77@gmail.com>
2014-06-02 18:58:11 +0300

    UI improvements

--- web/public/javascripts/directives/directives.js
+++ web/public/javascripts/directives/directives.js
@@ -9,14 +9,24 @@ define(
     var template  =
       '<div class="page_item" ng-mouseenter="editing=true" ng-mouseleave="editing=false">' +
         '<article class="page_item_content">' +
-          '<div ng-show="editing" ng-click="deleteItem({itemId:item._id})"> <span>Delete</span></div>' +
-          '<div class="page_item_image_wrap" ng-show="item.images.length > 0">' +
-            '<img class="page_item_image" ng-src={{item.images[0].url}}>' +
-          '<div class="page_item_title">' +
-            '<p>' +
-              '<a href="{{item.url}}" title="{{item.title}}"> {{item.title}} </p>' +
-            '</p>' +
+          //------item header-----
+          '<div class="page_item_header">'+
+            '<div ng-show="editing" ng-click="deleteItem({itemId:item._id})"> <span>Delete</span></div>' +
+          '</div>'+
+          //------item body-------
+          '<div class="page_item_body">' +
+            '<div class="page_item_image_wrap" ng-show="item.images.length > 0">' +
+              '<img class="page_item_image" ng-src={{item.images[0].url}}>' +
+            '</div>' +
           '</div>' +
+          //-----item footer------
+          '<div class="page_item_footer">' +
+            '<div class="page_item_title">' +
+              '<p>' +
+                '<a href="{{item.url}}" title="{{item.title}}"> {{item.title}} </p>' +
+              '</p>' +
+            '</div>' +
+          '</div>'+
         '</article>'+
       '</div>'
 
