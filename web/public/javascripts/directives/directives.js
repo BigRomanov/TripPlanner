d7d574add9108975873a26735c9a3e63c11fd57d
lirco <lirco77@gmail.com>
2014-06-02 17:06:34 +0300

    frontend layout of item improved

--- web/public/javascripts/directives/directives.js
+++ web/public/javascripts/directives/directives.js
@@ -8,14 +8,28 @@ define(
 
     // TODO: once used isolated scope - item names should be changed
     var template  =
-      '<div class="page_item">' +
-        '<div class="page_item_title">' +
-          '<a href="{{item.url}}" title="{{item.url}}"> {{item.title}} </p>' +
-        '</div>' +
-        '<div class="page_item_image">' +
-          '<img ng-src={{item.imageUrl}}>' +
-        '</div>' +
-      '</div>';
+      '<div class="page_item", style="margin:10px; width:360px">' +
+        '<article class="page_item_content">' +
+          //'<img class="page_item_image", ng-src={{item.imageUrl}} alt>' +
+          '<img class="page_item_image", ng-src="http://s3-eu-west-1.amazonaws.com/jamieoliverprod/_int/rdb2/upload/1076_1_1396364592_lrg.jpg" alt>' +
+          '<div class="page_item_title">' +
+            '<p>' +
+              '<a href="{{item.url}}" title="{{item.title}}"> {{item.title}} </p>' +
+            '</p>'
+          '</div>' +
+        '</article>'+
+      '</div>'
+
+
+
+
+//        '<div class="page_item_title">' +
+//
+//        '</div>' +
+//        '<div class="page_item_image">' +
+//          '<img ng-src={{item.imageUrl}}>' +
+//        '</div>' +
+//      '</div>';
 
     return {
       restrict: 'E',
