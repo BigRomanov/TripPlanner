a55fdfa65b1d66a37f595ecee3c99ed089343518
lirco <lirco77@gmail.com>
2014-06-26 19:46:22 +0300

    item now as directive

--- web/public/javascripts/directives/directives.js
+++ web/public/javascripts/directives/directives.js
@@ -7,36 +7,31 @@ define(
   tripApp.directive( 'tripItem', function($compile, $timeout) {
 
     var template  =
-      '<div class="page_item" ng-mouseenter="editing=true" ng-mouseleave="editing=false">' +
-        '<article class="page_item_content">' +
-          //------item header-----
-          '<div class="page_item_header">'+
-            '<div ng-show="editing" ng-click="deleteItem({itemId:item._id})"> <span>Delete</span></div>' +
-          '</div>'+
-          //------item body-------
-          '<div class="page_item_body">' +
-            '<div class="page_item_image_wrap" ng-show="item.images.length > 0">' +
-              '<img class="page_item_image" ng-src={{item.images[0].url}}>' +
-            '</div>' +
-          '</div>' +
-          //-----item footer------
-          '<div class="page_item_footer">' +
-            '<div class="page_item_title">' +
-              '<p>' +
-                '<a href="{{item.url}}" title="{{item.title}}"> {{item.title}} </p>' +
-              '</p>' +
-            '</div>' +
-          '</div>'+
-        '</article>'+
+
+      '<div class="itemMeta">' +
+        '<p style="font-weight:bold">{{item.title}}</p>' +
+        <!--p(ng-if="counter!=0") {{item.url}}-->
+      '</div>' +
+      '<div class="itemActionsWrapper">' +
+        '<div class="itemImageWrapper">' +
+          '<img ng-src="{{item.images[0].url}}" class="itemImg"/>' +
+          '<ul ng-show="showButtons" class="itemButtonsWrapper">' +
+            '<li class="ButtonWrapper">' +
+              '<button ng-href="" ng-click="deleteItem(item)" class="itemBtn">X</button>' +
+            '</li>' +
+            '<li class="ButtonWrapper">' +
+              '<button ng-href="" ng-click="click(item)" class="itemBtn">T</button>' +
+            '</li>' +
+          '</ul>' +
+        '</div>' +
+      '</div>' +
+      '<div class="itemDescriptionWrapper">' +
+        '<p>hi there. this is some description for test. are you satisfied with what you see here?</p>'
       '</div>'
 
 
     return {
       restrict: 'E',
-      scope: {
-        item: '=',
-        deleteItem: '&'
-      },
       template: template
     };
   });
